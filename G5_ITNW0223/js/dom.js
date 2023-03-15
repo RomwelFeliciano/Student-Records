06f7de72505becc2589717a20ba82a16e783ecf2
Romwel Feliciano <127919521+RomwelFeliciano@users.noreply.github.com>
2023-03-15 21:38:51 +0800

    Add files via upload

---
+++ G5_ITNW0223/js/dom.js
@@ -0,0 +1,72 @@
+//Size
+function changeTextSize(size)
+{	
+    document.getElementById("p").style.fontSize = size;
+    document.getElementById("vision").style.fontSize = size;
+    document.getElementById("mission").style.fontSize = size;
+}
+
+//Color
+function changeTextColor(color)
+{	
+	document.getElementById("h1").style.color = color;
+    document.getElementById("p").style.color = color;
+    document.getElementById("p2").style.color = color;
+    document.getElementById("vision").style.color = color;
+    document.getElementById("mission").style.color = color;
+    document.getElementById("Size").style.color = color;
+    document.getElementById("Color").style.color = color;
+    document.getElementById("Alignment").style.color = color;
+    document.getElementById("Font").style.color = color;
+    document.getElementById("Style").style.color = color;
+}
+
+//Alignment
+function changeTextAlign(align)
+{
+    document.getElementById("h1").style.textAlign = align;
+    document.getElementById("p").style.textAlign = align;
+    document.getElementById("vision").style.textAlign = align;
+    document.getElementById("mission").style.textAlign = align;
+}
+
+//Font
+function changeTextFont(font)
+{	   
+    document.getElementById("h1").style.fontFamily = font;
+    document.getElementById("p").style.fontFamily = font;
+    document.getElementById("p2").style.fontFamily = font;
+    document.getElementById("vision").style.fontFamily = font;
+    document.getElementById("mission").style.fontFamily = font;
+    document.getElementById("Size").style.fontFamily = font;
+    document.getElementById("Color").style.fontFamily = font;
+    document.getElementById("Alignment").style.fontFamily = font;
+    document.getElementById("Font").style.fontFamily = font;
+    document.getElementById("Style").style.fontFamily = font;
+    
+}
+
+//Style
+function changeTextDesign(style)
+{
+    document.getElementById("h1").style.fontWeight = style;
+    document.getElementById("h1").style.fontStyle = style;
+    document.getElementById("p").style.fontWeight = style;
+    document.getElementById("p").style.fontStyle = style;
+    document.getElementById("p2").style.fontWeight = style;
+    document.getElementById("p2").style.fontStyle = style;
+    document.getElementById("vision").style.fontWeight = style;
+    document.getElementById("vision").style.fontStyle = style;
+    document.getElementById("mission").style.fontWeight = style;
+    document.getElementById("mission").style.fontStyle = style;
+    document.getElementById("Size").style.fontWeight = style;
+    document.getElementById("Size").style.fontStyle = style;
+    document.getElementById("Color").style.fontWeight = style;
+    document.getElementById("Color").style.fontStyle = style;
+    document.getElementById("Alignment").style.fontWeight = style;
+    document.getElementById("Alignment").style.fontStyle = style;
+    document.getElementById("Font").style.fontWeight = style;
+    document.getElementById("Font").style.fontStyle = style;
+    document.getElementById("Style").style.fontWeight = style;
+    document.getElementById("Style").style.fontStyle = style;
+}
